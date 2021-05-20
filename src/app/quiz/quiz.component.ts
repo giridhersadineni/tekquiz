7efeaea57ca13f04822552530d900cb198a17058
Giridher Sadineni <giridher@hotmail.com>
2021-05-20 16:11:57 +0530

    done changes to finish routes

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,46 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from '../quiz.service';
-import{ActivatedRoute} from '@angular/router';
-import {Subscription,interval} from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-  export class QuizComponent implements OnInit {
-  
-  public questions:any=[];
-  public totalquestions:number;
-  public totalanswered:number;
+export class QuizComponent implements OnInit {
+
+  public questions: any = [];
+  public totalquestions: number;
+  public totalanswered: number;
   public initialQuestion;
-  public time:number;
-  public timer; 
+  public time: number;
+  public timer;
   private subscription: Subscription;
-  constructor(private service:QuizService,private route:ActivatedRoute) { 
-    this.totalquestions=this.service.getTotalQuestions();
-    this.totalanswered=this.service.getTotalAnswered();
-    this.initialQuestion=this.service.nextQuestion();
-  }
-  
-  ngOnInit(): void {
-    this.time=this.totalquestions*30-1;
-    this.subscription=interval(1000)
-    .subscribe(x=>{ this.tick(); });
-    this.initialQuestion=this.service.nextQuestion();
+
+  constructor(private service: QuizService, private route: ActivatedRoute, private router: Router) {
+    this.totalquestions = this.service.getTotalQuestions();
+    this.totalanswered = this.service.getTotalAnswered();
+    this.initialQuestion = this.service.nextQuestion();
   }
 
-  tick(){
-    this.totalanswered=this.service.getTotalAnswered();
-   if(this.time>0){
-     this.time--;
-   }
-   else if(this.time==0){
-     this.subscription.unsubscribe();
-     clearInterval(this.timer);
-     alert("Time Up");
-     this.endQuiz();
-   }
+  ngOnInit(): void {
+    this.time = this.totalquestions * 1;
+    this.subscription = interval(1000)
+      .subscribe(x => { this.tick(); });
+    this.initialQuestion = this.service.nextQuestion();
+  }
+
+  tick() {
+    this.totalanswered = this.service.getTotalAnswered();
+    if (this.time > 0) {
+      this.time--;
+    }
+    else if (this.time == 0) {
+      this.subscription.unsubscribe();
+      clearInterval(this.timer);
+      alert("Time Up");
+      this.router.navigate([ "finishquiz" ]);
+    }
   }
 
 
@@ -48,17 +49,17 @@ import {Subscription,interval} from 'rxjs';
 
 
   endQuiz() {
-    let submit=confirm("Do you want to Finish Exam and Submit");    
-    if(submit){
-      window.location.href='finishquiz';
+    let submit = confirm("Do you want to Finish Exam and Submit");
+    if (submit) {
+      window.location.href = 'finishquiz';
     }
-    else{
+    else {
 
     }
 
   }
 
-  
+
 
 
 }
